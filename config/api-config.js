070514327973ce8374e91fef8b18e4bddcee7e5a
Jared <34344327+jared0612@users.noreply.github.com>
2026-05-09 19:50:47 +0800

    Modify API_URL and MODEL in api-config.js
    Updated API configuration to use new URL and model.

--- config/api-config.js
+++ config/api-config.js
@@ -3,8 +3,8 @@ export const CONFIG = {
     // API 配置
     API: {
         DEEPSEEK_API_KEY: 'xxx',	//修改项
-        API_URL: ' https://api.deepseek.com/v1',
-        MODEL: 'deepseek-chat'	//修改项
+        API_URL: 'https://api.deepseek.com',    //多关注官网文档
+        MODEL: 'DeepSeek-V4-Flash'	//修改项，注意：deepseek-chat (将于 2026/07/24 弃用) deepseek-reasoner (将于 2026/07/24 弃用)
     }
 
 };
